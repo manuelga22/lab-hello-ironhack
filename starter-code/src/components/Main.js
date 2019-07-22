8378644624b828bd8bf6da9c3bff3e7a35a2fe06
Manuel Garcia <mgarc881@fiu.edu>
2019-07-22 15:09:13 -0400

    done

---
+++ starter-code/src/components/Main.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import '../App.css';
+
+class Main extends Component {
+render(){
+  return(
+    <div>
+       <nav>
+       </nav>
+       <div id = "stuff">
+         <h2>Say hello to React.js</h2>
+         <p>You will learn a frontend<br></br>
+         framework from scratch, to<br></br> 
+         become an ninka developer</p>
+         <button>Awesome!</button>
+       </div>
+
+
+    </div>
+  )
+
+}
+
+}
+export default Main;
